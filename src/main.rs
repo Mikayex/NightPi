8c9e3e2907563523e0ba8513b17af098b92ffe36
Thomas Laroche <tho.laroche@gmail.com>
2020-06-15 22:06:51 +0200

    Add sample webserver

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let hello = warp::path!(String).map(|path| format!("You called /{}", path));
+    warp::serve(hello).run(([0, 0, 0, 0], 3030)).await;
 }
