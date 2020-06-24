b1580e24a39df289478cfd3f4a3cc8feb96d113b
Thomas Laroche <tho.laroche@gmail.com>
2020-06-24 17:29:37 +0200

    Refactor system::reboot and system::poweroff

--- src/system/mod.rs
+++ src/system/mod.rs
@@ -1,15 +1,13 @@
 use crate::root;
 use log::info;
 use std::io;
-use std::process::Command;
+use std::process::{Command, Output};
 
-pub fn reboot() -> Result<(), io::Error> {
-    info!("Reboot requested");
-    let mut cmd = Command::new("reboot");
-    root::execute_as_root(|| match cmd.output() {
+fn execute_command(cmd: &mut Command) -> Result<Output, io::Error> {
+    match cmd.output() {
         Ok(output) => {
             if output.status.success() && output.stderr.is_empty() {
-                return Ok(());
+                return Ok(output);
             }
             Err(io::Error::new(
                 io::ErrorKind::Other,
@@ -17,22 +15,17 @@ pub fn reboot() -> Result<(), io::Error> {
             ))
         }
         Err(error) => Err(error),
-    })?
+    }
+}
+
+pub fn reboot() -> Result<(), io::Error> {
+    info!("Reboot requested");
+    let mut cmd = Command::new("reboot");
+    root::execute_as_root(|| execute_command(&mut cmd))?.and(Ok(()))
 }
 
 pub fn poweroff() -> Result<(), io::Error> {
     info!("Poweroff requested");
     let mut cmd = Command::new("poweroff");
-    root::execute_as_root(|| match cmd.output() {
-        Ok(output) => {
-            if output.status.success() && output.stderr.is_empty() {
-                return Ok(());
-            }
-            Err(io::Error::new(
-                io::ErrorKind::Other,
-                String::from_utf8(output.stderr).unwrap(),
-            ))
-        }
-        Err(error) => Err(error),
-    })?
+    root::execute_as_root(|| execute_command(&mut cmd))?.and(Ok(()))
 }
