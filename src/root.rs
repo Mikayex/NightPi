9b0a14c0449d5633ccf6cc28df3041322f2298a0
Thomas Laroche <tho.laroche@gmail.com>
2020-06-26 15:09:57 +0200

    Set supplementary groups when dropping root

--- src/root.rs
+++ src/root.rs
@@ -1,6 +1,22 @@
 use crate::config::Config;
 use users::switch::{set_effective_gid, set_effective_uid, switch_user_group};
 
+fn setgroups(groups: &[users::Group]) -> Result<(), std::io::Error> {
+    let gids: Vec<libc::gid_t> = groups.iter().map(|group| group.gid()).collect();
+    let res = unsafe {
+        libc::setgroups(
+            gids.len() as libc::size_t,
+            gids.as_ptr() as *const libc::gid_t,
+        )
+    };
+
+    match res {
+        0 => Ok(()),
+        -1 => Err(std::io::Error::last_os_error()),
+        n => unreachable!("setegid returned {}", n),
+    }
+}
+
 pub fn user_is_root() -> bool {
     users::get_effective_uid() == 0
 }
@@ -20,16 +36,6 @@ pub fn drop_root(config: &Config) -> Result<(), String> {
         return Ok(());
     }
 
-    //Drop group first as it may not possible to do after dropping user
-    let groupname = config
-        .group
-        .as_ref()
-        .ok_or_else(|| "No group provided".to_string())?;
-    let gid = users::get_group_by_name(&groupname)
-        .ok_or_else(|| format!("No group {} found", groupname))?
-        .gid();
-    set_effective_gid(gid).or_else(|err| Err(err.to_string()))?;
-
     let username = config
         .user
         .as_ref()
@@ -37,6 +43,25 @@ pub fn drop_root(config: &Config) -> Result<(), String> {
     let uid = users::get_user_by_name(&username)
         .ok_or_else(|| format!("No user {} found", username))?
         .uid();
+
+    let groupname = config
+        .group
+        .as_ref()
+        .ok_or_else(|| "No group provided".to_string())?;
+    let gid = users::get_group_by_name(&groupname)
+        .ok_or_else(|| format!("No group {} found", groupname))?
+        .gid();
+
+    //Set supplementary groups
+    let sup_groups: Vec<users::Group> = users::get_user_groups(&username, gid)
+        .unwrap_or_default()
+        .into_iter()
+        .filter(|group| gid == 0 || group.gid() != 0)
+        .collect();
+    setgroups(&sup_groups).or_else(|err| Err(err.to_string()))?;
+
+    //Drop group first as it may not possible to do after dropping user
+    set_effective_gid(gid).or_else(|err| Err(err.to_string()))?;
     set_effective_uid(uid).or_else(|err| Err(err.to_string()))?;
 
     Ok(())
